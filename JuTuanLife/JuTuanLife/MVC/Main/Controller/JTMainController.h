cd62925d9895872c70d2c585be2e6b2797e373bf
cheng <wangzhicheng@chelun.com>
2020-03-04 02:00:38 +0800

    update

---
+++ JuTuanLife/JuTuanLife/MVC/Main/Controller/JTMainController.h
@@ -0,0 +1,17 @@
+//
+//  JTMainController.h
+//  JuTuanLife
+//
+//  Created by cheng on 2020/3/4.
+//  Copyright © 2020 cheng. All rights reserved.
+//
+
+#import <WCBaseUIKit/WCBaseUIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JTMainController : DTViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
